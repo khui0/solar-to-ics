ae518dd3c89bd0ba507fa76c33ba31b877ccf9e1
Kenny <101839505+khui0@users.noreply.github.com>
2026-01-28 14:26:26 -0500

    feat: add meta desc

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,11 @@
 <svelte:head>
   <link rel="icon" href={favicon} />
   <title>SOLAR to iCalendar</title>
+  <meta
+    name="description"
+    content="This tool takes your SBU class schedule and turns it into a file you can import into Apple
+    Calendar, Google Calendar, or any other calendar software."
+  />
 </svelte:head>
 <div class="mx-auto flex max-w-xl flex-col gap-3 p-4">
   {@render children()}
